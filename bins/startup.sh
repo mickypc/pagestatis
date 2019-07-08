afc041e038d0730ce28d6c1a5f5fdf38b71aa17d
micky4aliyun <mickypc@cuc.edu.cn>
2019-07-08 20:27:24 +0800

    add test jetty undertow

--- bins/startup.sh
+++ bins/startup.sh
@@ -3,7 +3,8 @@ version=$1
 if [ "$version" != "" ]
 then
 	basedir=$(cd "$(dirname "$0")";pwd)
-	nohup java -jar -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=128m -Xms1024m -Xmx1024m -Xmn256m -Xss256k -XX:SurvivorRatio=8 -XX:+UseConcMarkSweepGC ${basedir}/pagestatis-${version}.jar >> ${basedir}/nohup.out 2>&1 &
+	nohup java -server -jar -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=128m -Xms1024m -Xmx1024m -Xmn256m -Xss256k -XX:SurvivorRatio=8 -XX:+UseConcMarkSweepGC ${basedir}/pagestatis-${version}.jar >> ${basedir}/nohup.out 2>&1 &
+#	nohup java -server -jar ${basedir}/pagestatis-${version}.jar >> ${basedir}/nohup.out 2>&1 &
 else
 	echo "usage: $0 <version>"
 fi
